104c28b300f9b2d400706d4b6810a04ab809b18a
sarkerrakib <freelancerrakib25@gmail.com>
2022-02-17 23:47:15 +0600

    js-function

---
+++ first.js
@@ -0,0 +1,15 @@
+function valuIncome(id) {
+    const Input = document.getElementById(id);
+    const Input2Number = parseFloat(Input.value);
+    // Input.value = ''
+    return Input2Number
+
+}
+function idSelector(id) {
+    return AmountHtml = document.getElementById(id)
+}
+
+
+
+let totalAmount = 0
+
